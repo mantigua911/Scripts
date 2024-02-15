bbf7434ddf374f90ad188a3649c3427865e0a07c
mantigua911 <160046487+mantigua911@users.noreply.github.com>
2024-02-15 15:13:23 -0500

    Deleted start-powershell and added new test file

---
+++ run-ps-admin.ps1
@@ -0,0 +1,15 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+cd \
+	if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript")
+ 		{ $ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition }
+ 	else
+ 		{ $ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0]) 
+     if (!$ScriptPath){ $ScriptPath = "." } }
+$ScriptPath
+cd $ScriptPath
