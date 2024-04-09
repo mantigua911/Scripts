ad6bed179eb8d7672ce26d11b3be81752684465f
mantigua911 <160046487+mantigua911@users.noreply.github.com>
2024-04-09 09:34:24 -0400

    Update Sync-ALL-iOS.ps1

--- Sync-ALL-iOS.ps1
+++ Sync-ALL-iOS.ps1
@@ -5,19 +5,18 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
   Break
 }
 
-Connect-MSGraph
+
+Connect-MSGraph -AdminConsent
 Write-Host "Pulling Audio Guide information. Please wait...`n" 
 
 $DevicesToSync = Get-IntuneManagedDevice | Get-MSGraphAllPages | where-object {($_.managementAgent -eq 'mdm' ) -and ($_.DeviceName -Like "VSAG*")}
-
- 
 
 Write-Host "Would you like to do it in BATCHES or ALL devices?(B for Batches/A for All)"
 $ans = Read-Host
 Start-Sleep -Seconds 3
 
 if ($ans.toUpper() -eq "B")
-{
+    {
 	Write-Host "The last number being currently used is $($DevicesToSync.DeviceName[-1])"
 	Start-Sleep -Seconds 2
 
@@ -27,7 +26,7 @@ if ($ans.toUpper() -eq "B")
 	Foreach ($Device in $DevicesToSync)
 	{
   		$SplitDevice = $Device.DeviceName.Split("G")
-		If (($SplitDevice[-1] -gte $start) -and ($SplitDevice[-1] -lte $end)) 
+		If (($SplitDevice[-1] -ge $start) -and ($SplitDevice[-1] -le $end)) 
 		{
 			Invoke-IntuneManagedDeviceSyncDevice -managedDeviceID $Device.managedDeviceId
 	Write-Host "Sending Sync request to Device with Name $($Device.deviceName)" -ForegroundColor Green
@@ -41,3 +40,4 @@ if ($ans.toUpper() -eq "B")
 	Write-Host "Sending Sync request to Device with Name 	$($Device.deviceName)" -ForegroundColor Green
   	}
 }
+Read-Host "error?"
