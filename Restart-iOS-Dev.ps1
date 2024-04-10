ea86fffc7865221cc42906f764fb67a9e5167f19
mantigua911 <160046487+mantigua911@users.noreply.github.com>
2024-04-10 08:45:57 -0400

    Sync + Restart IOS Intune
    - Created Restart Script for devices
    - Improved Sync Devices for Intune

--- Restart-iOS-Dev.ps1
+++ Restart-iOS-Dev.ps1
@@ -9,7 +9,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 Connect-MSGraph -AdminConsent
 Write-Host "Pulling Audio Guide information. Please wait...`n" 
 
-$DevicesToSync = Get-IntuneManagedDevice | Get-MSGraphAllPages | where-object {($_.managementAgent -eq 'mdm' ) -and ($_.DeviceName -Like "VSAG*")}
+$DevicesToRestart = Get-IntuneManagedDevice | Get-MSGraphAllPages | where-object {($_.managementAgent -eq 'mdm' ) -and ($_.DeviceName -Like "VSAG*")}
 
 Write-Host "Would you like to do it in BATCHES, ALL devices, or for a SINGLE device?(B for Batches/A for All/S for Single)"
 $ans = Read-Host
@@ -18,19 +18,19 @@ Start-Sleep -Seconds 3
 switch ($ans.ToUpper()) {
 	"B" 
 	{ 
-		Write-Host "The last number being currently used is $($DevicesToSync.DeviceName[-1])"
+		Write-Host "The last number being currently used is $($DevicesToRestart.DeviceName[-1])"
 		Start-Sleep -Seconds 2
 	
 		$start = Read-Host "AG number to beginning at"
 		$end = Read-Host "AG number to end at"
 	
-		Foreach ($Device in $DevicesToSync)
+		Foreach ($Device in $DevicesToRestart)
 		{
 				$SplitDevice = $Device.DeviceName.Split("G")
 			If (($SplitDevice[-1] -ge $start) -and ($SplitDevice[-1] -le $end)) 
 			{
-				Invoke-IntuneManagedDeviceSyncDevice -managedDeviceID $Device.azureADDeviceId
-				Write-Host "Sending Sync request to Device with Name $($Device.deviceName)" -ForegroundColor Green
+				Invoke-IntuneManagedDeviceRebootNow -managedDeviceID $Device.azureADDeviceId
+				Write-Host "Sending restart request to Device with Name $($Device.deviceName)" -ForegroundColor Green
 			}
 	  
 		}
@@ -38,22 +38,22 @@ switch ($ans.ToUpper()) {
 	"S" 
 	{
 		$valToCheck = Read-Host -Prompt "Which device would you like to SYNC?(ONLY the Numbers)"
-		Foreach ($Device in $DevicesToSync)
+		Foreach ($Device in $DevicesToRestart)
 		{
 			$SplitDevice = $Device.DeviceName.Split("G")
 			if ($($SplitDevice[-1] -eq $valToCheck)) {
-				Invoke-IntuneManagedDeviceSyncDevice -managedDeviceId $Device.azureADDeviceId
-				Write-Host "Sending Sync request to Device with Name $($Device.deviceName)" -ForegroundColor Green
+				Invoke-IntuneManagedDeviceRebootNow -managedDeviceId $Device.azureADDeviceId
+				Write-Host "Sending restart request to Device with Name $($Device.deviceName)" -ForegroundColor Green
 			}
 		}	
 	}
 	Default 
 	{
-		Foreach ($Device in $DevicesToSync)
+		Foreach ($Device in $DevicesToRestart)
 		{
-			Invoke-IntuneManagedDeviceSyncDevice -managedDeviceId $Device.azureADDeviceId
-			Write-Host "Sending Sync request to Device with Name $($Device.deviceName)" -ForegroundColor Green
+			Invoke-IntuneManagedDeviceRebootNow -managedDeviceId $Device.azureADDeviceId
+			Write-Host "Sending restart request to Device with Name $($Device.deviceName)" -ForegroundColor Green
 		}
 	}
 }
-Read-Host "Completed"
+Read-Host "Completed!"
